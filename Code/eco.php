271c8215f17b49dfd836fde6678cf296af386a33
jeremy <jeremy.egremy@gmail.com>
2019-11-27 21:15:23 +0100

    page éco
    page économique + mise à jour tâche perso

---
+++ Code/eco.php
@@ -0,0 +1,139 @@
+<html lang="fr">
+	<head>
+		<title></title>
+		<meta charset="utf-8">
+
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,400&display=swap" rel="stylesheet"> 
+
+		<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+
+		<script type="text/javascript" src="assets/js/jquery-3.4.1.js"></script>
+		<script type="text/javascript" src="assets/js/bootstrap.js"></script>
+		<script type="text/javascript" src="assets/js/bootstrap.bundle.js"></script>
+		<script type="text/javascript" src="assets/js/main.js"></script>
+
+		<script src="assets/js/main.js"></script>
+
+        <title>TakeTwoInteractive - Acceuil</title>
+	</head>
+	<body data-spy="scroll" data-target="#navbar-main" class="background-image-auto background-image bg3_image">
+		<header> <?php include("navbar.html"); ?> </header>
+
+		<section id="theinsperationalquote" class="quote-block" style="">
+			<div class="container material-light">
+				<div class="row">
+					<div class="col">	
+						<p style="font-size: 0.8em;">Ici vous trouverez plus d'informations sur l’entreprise Take-Two Interactive d’un point de vue économique.</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="arrow-wrapper" onclick="scrollto('pagestart')">
+				<p class="arrowtext">Voir plus</p>
+				<img src="assets/img/arrow.svg" class="arrowimg animated bounce">
+			</div>
+		</section>
+
+
+		<header class="margin" id="pagestart">
+		</header>
+
+		<section id="games" class="">
+			<div class="container2 material-light">
+				<div class="row">
+					<div class="col col-text">	
+						<h2>L'entreprise</h2>
+						<p class="lead">
+							L’entreprise Take-Two Interactive est cotée à la bourse de New-York (TTWO), c’est une Limited Liability Company(LLC) ce qui est l'équivalent d’une Société À Responsabilité Limitée(SARL) en France. Ce type d’entreprise bénéficie de quelques avantages comme : 
+						
+							<ul>
+								<li>le choix de son éligibilité à l’imposition,</li>
+								<li> la responsabilité limitée protège, en fonction des lois sur la protection de l'État, les propriétaires, aussi appelé “membres” de l’entreprise, des actes et/ou dettes de la LLC,</li>
+								<li>risque diminué d’être victime de vole par le “fire sale”(revente à des prix extrêmement bas).</li>
+							</ul>
+							Mais les LLC ont également certains inconvénients comme :
+							<ul>
+								<li>des difficultés à lever des capitaux financiers,</li>
+								<li>les frais de renouvellement peuvent être relativement élevés,</li>
+								<li>la structure de l’entreprise peut être difficile à comprendre pour beaucoup.</li>
+							</ul>
+						</p>
+					</div>
+					<div class="col col-img">
+						<img src="assets/img/Take-Two-eco.png"/>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="creators" class="">
+			<div class="container2 material-light">
+				<div class="row">
+					<div class="col col-img">
+						<img src="assets/img/test.png"/>
+					</div>
+					<div class="col col-text">	
+						<h2>Quelques chiffres :</h2>
+						<p>
+							Take-Two Interactive c’est : 
+							<ul>
+								<li>2 332 millions € de chiffre d’affaires en 2019,</li>
+								<li>1 792 millions € de chiffre d’affaires en 2018,</li>
+								<li>3 400 employés,</li>
+								<li>19 studios de développement dans le monde,</li>
+								<li>environ 120$ de valeur cotée à la bourse de New York.</li>
+							</ul>
+							Mais c’est aussi une entreprise concurrente de :
+
+							<ul>
+								<li>
+									Activision/Blizzard :
+									<ul>
+										<li>7 262 millions € de chiffre d’affaires en 2018,</li>
+										<li>9 900 employés,</li>
+										<li>des licences comme Call of Duty, Diablo ou encore World of Warcraft.</li>
+									</ul>
+								</li>
+								<li>
+									Ubisoft :
+									<ul>
+										<li>1 845 millions de chiffre d’affaires en 2018,</li>
+										<li>15 985 employés,</li>
+										<li>des licences comme Rainbow Six, Far Cry ou encore Assassin’s Creed.</li>
+									</ul>
+								</li>
+								<li>
+									CD Projekt :
+									<ul>
+										<li>85 millions de chiffre d’affaires en 2018,</li>
+										<li>600 employés,</li>
+										<li>des licences comme The Witcher ou Cyberpunk.</li>
+									</ul>
+								</li>
+							</ul>
+						</p>					
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="thune" class="">
+			<div class="container2 material-light">
+				<div class="row">
+					<div class="col col-text">	
+						<h2>L'évolution</h2>
+						<p class="lead">Take-Two Interactive est un des leaders du monde de la création de jeu vidéo, devançant comme vue ci-dessus Ubisoft et CD Projekt en termes de chiffre d’affaires. Ce géant doit son succès à des jeux phénomène tel que GTA V classé comme le 3ème jeu le plus vendu au monde avec plus de 110 millions de ventes depuis 2015, et bien d’autre licences que vous pouvez découvrir dans la partie “notre vitrine”.
+						l’entreprisereprise a connue une grande croissance au niveau du chiffre d’affaires et a battue des records de vente, l’avenir de Take-Two Interactive semble être dans la même direction que son évolution jusqu'à aujourd'hui, de plus une rumeur s'était répandue sur un potentiel achat de Take-Two Interactive par le géant Sony en mars 2019.
+
+						</p>
+					</div>
+					<div class="col col-img">
+						<img src="assets/img/graphe.png"/>
+					</div>
+				</div>
+			</div>
+		</section>
+
+        <?php  include("footer.html"); ?>
+	</body>
+</html>
