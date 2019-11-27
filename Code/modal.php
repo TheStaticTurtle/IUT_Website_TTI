56a27f35c0c19d96ce5fac68b723782224cdba93
TurtleForGaming <tuglersamuel@gmail.com>
2019-11-27 16:26:57 +0100

    Adding modal popup to images

--- Code/modal.php
+++ Code/modal.php
@@ -1,17 +1,17 @@
 <div class="modal fade" id="modal_<?php echo $id; ?>" tabindex="-1" role="dialog" aria-labelledby="modal_<?php echo $id; ?>" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content material-primary rounded-0">
-      <div class="modal-header material-dark p-0">
-        <h5 class="modal-title" id="exampleModalCenterTitle"><?php echo $title; ?></h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+      <div class="modal-dialog modal-dialog-centered rounded-0" role="document">
+            <div class="modal-content ">
+                  <div class="modal-header material-dark ">
+                        <h5 class="modal-title" id="exampleModalCenterTitle"><?php echo $title; ?></h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                        </button>
+                  </div>
+                  <div class="modal-body material-primary">
+                        <img src="<?php echo $src; ?>">
+                  </div>
+            </div>
       </div>
-      <div class="modal-body">
-        <img src="<?php echo $src; ?>">
-      </div>
-    </div>
-  </div>
 </div>
 
 <script>
