cca4a78a478f006df25e5794e71e06ffd0a7c9e5
Orimius <55831728+Orimius@users.noreply.github.com>
2019-11-27 16:13:24 +0100

    Last Changes (27/11)

--- Code/jeuxphares.php
+++ Code/jeuxphares.php
@@ -101,7 +101,7 @@
 						<p class="lead" style="font-size: 2em; font-style: italic;">
 							NBA 2K, Borderlands, Mafia...
 						</p>
-						<p style="font-size: 1.29em; text-align: justify;">
+						<p style="font-size: 1.28em; text-align: justify;">
 							Fondé le 25 janvier 2005, éditeur de jeux vidéos, 2K s'est lui aussi imposé dans l'industrie du jeu vidéo avec des licences célèbres comme NBA 2K ou encore Borderlands développé par Gearbox.
 							<br>
 							Filiale de Take-Two Interactive, leur siège social étant situé à Novato dans l'état de la Californie aux États-Unis.
@@ -123,23 +123,20 @@
 				<div class="row">
 					<div class="col col-img">
 						<img src="assets/img/JeuxPhares.jpg"/>
+						
 					</div>
 					<div class="col col-text">
 						<h2 style="font-size: 4em; font-weight: bold;">
 							Les principales franchises
 						</h2>
-						<p style="font-size: 1.29em; text-align: justify;">
-							La licence Grand Theft Auto a permis à Rockstar de s'approprier un grand renom dans l'univers vidéoludique. GTA V, jeu d'action-aventure en monde ouvert, est le jeu le plus vendu de Take-Two Interactive, est aussi un succès critique dès sa sortie. Il s'est crée une communauté au fur et à mesure des années grâce à son multijoueur très popularisé.
+						<p style="font-size: 1.2em; text-align: justify;">
+							La licence Grand Theft Auto a permis à Rockstar de s'approprier un grand renom dans l'univers vidéoludique. GTA V, jeu d'action-aventure en monde ouvert dans une ville fictive inspirée de villes "réelles", est le jeu le plus vendu de Take-Two Interactive et est aussi un succès critique dès sa sortie. Il s'est crée une communauté au fur et à mesure des années grâce à son multijoueur très popularisé.
 							<br>
-							Red Dead Redemption est un jeu d'action-aventure en monde ouvert dans une ambiance "western", le premier opus s'est vendu
+							Red Dead Redemption est un jeu d'action-aventure en monde ouvert dans une ambiance "western", lui aussi a pu avoir un très bon accueil du public. Il y a quelques similarités par rapport à GTA qui est lui aussi un jeu en monde ouvert.
 							<br>
-							
+							La saga NBA 2K a permis à 2K de se faire un grand nom dans les jeux vidéos de sport. C'est très clairement la saga de jeux vidéos de basketball la plus populaire, leurs jeux ont beaucoup de succès grâce à son "gameplay" atypique, sa communauté et à la communication avant sa sortie. En effet, chaque année et pour chaque opus, un joueur de basketball est affiché sur la jaquette du jeu. Et pour les différentes éditions spéciales, ce sont des joueurs "légendaires" qui sont affichés (par exemple, LeBron James sur l'édition 20ème anniversaire de NBA 2K19).
 							<br>
-							Les deux frères ont quand même prêté leur voix à quelques jeux. Par exemple :
-							<br>
-							- Sam Houser : gangster sur GTA: San Andreas et piéton sur GTA IV
-							<br>
-							- Dan Houser : GTA III, GTA: Vice City, GTA IV
+							Borderlands est un jeu vidéo mêlant FPS et RPG. La série Borderlands est composé de 4 opus dont le 3ème qui est sorti dernièrement le 13 septembre. Chaque opus a été développé par Gearbox Software. Très apprécié par la communauté grâce à son univers est la possibilité de jouer en coopération, ce qui fait le charme de la série.
 						</p>					
 					</div>
 				</div>
