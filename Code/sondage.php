ccd3e56b3a1ed93f401b2c5be1182a034140c416
Lucas <starlucas68@laposte.net>
2019-11-27 11:22:39 +0100

    Page Sondage

--- Code/sondage.php
+++ Code/sondage.php
@@ -3,24 +3,33 @@
 		<title></title>
 		<meta charset="utf-8">
 
-		<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,400&display=swap" rel="stylesheet"> 
-
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,400&display=swap" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="assets/css/main.css">
 		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
-
-		<script type="text/javascript" src="assets/js/jquery-3.4.1.js"></script>
-		<script type="text/javascript" src="assets/js/bootstrap.js"></script>
-		<script type="text/javascript" src="assets/js/bootstrap.bundle.js"></script>
-		<script type="text/javascript" src="assets/js/main.js"></script>
-
-		<script src="assets/js/main.js"></script>
-
 	</head>
+
 	<body data-spy="scroll" data-target="#navbar-main" class="background-image bg3_image">
 		<header> <?php include("navbar.html"); ?> </header>
 
 		<section>
-
+            <div class="container2 material-light">
+                <p style="text-align: justify">
+                    Afin de récupérer les opinions du public envers l'entreprise Take-Two Interactive, nous avons décidés de réaliser un sondage.
+                    Ce dernier à été partagé aux autres élèves du DUT Informatique et à de diverses communautés via des serveurs Discord.
+                    Nous avons à ce jour récolté plus de 150 réponses avec une majorité des participants étant des hommes ayant entre 18 et 25 ans qui est le public principalement visé par l'entreprise.
+                </p>
+                <div class="row">
+                    <div class="col">
+                        <img src="assets/img/SONDAGE_1.png" alt="Résultat sondage connaissance" height="200px">
+                    </div>
+                    <div class="col">
+                        <img src="assets/img/SONDAGE_2.png" alt="Résultat sondage marques" height="200px">
+                    </div>
+                </div>
+                <p>
+                    On peut voir sur la 1ère image que plus de 46% des participants ne connaissaient pas l'entreprise Take-Two Interactive alors que plus de 90% des participants connaissent leur principale fillière Rockstar Games.
+                </p>
+            </div>
 		</section>
 
 		<footer class="py-5 bg-dark">
@@ -28,5 +37,10 @@
 				<p class="m-0 text-center text-white">Site réalisé par Samuel Tugler, Wassim Chalabi, Jérémy Egremy et Lucas Mathieu.</p>
 			</div>
 		</footer>
+
+        <script type="text/javascript" src="assets/js/jquery-3.4.1.js"></script>
+        <script type="text/javascript" src="assets/js/bootstrap.js"></script>
+        <script type="text/javascript" src="assets/js/bootstrap.bundle.js"></script>
+        <script type="text/javascript" src="assets/js/main.js"></script>
 	</body>
 </html>
