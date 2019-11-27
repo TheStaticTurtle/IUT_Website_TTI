2f00c300ca3a3d583c0913fe725b1f49c1655234
TurtleForGaming <tuglersamuel@gmail.com>
2019-11-27 16:19:19 +0100

    Adding modal popup to images

--- Code/sondage.php
+++ Code/sondage.php
@@ -5,6 +5,7 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,400&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="assets/css/main.css">
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <script type="text/javascript" src="assets/js/jquery-3.4.1.js"></script>
     <title>TakeTwoInteractive - Nos avis</title>
 </head>
 
