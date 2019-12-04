b83c989ba049f1d04a4f5927bf3a4019d8b6757d
TurtleForGaming <tuglersamuel@gmail.com>
2019-12-04 15:04:46 +0100

    Removed auto update of bg images

--- Code/eco.php
+++ Code/eco.php
@@ -11,7 +11,7 @@
 		<script type="text/javascript" src="assets/js/main.js"></script>
 		<script src="assets/js/main.js"></script>
 	</head>
-	<body data-spy="scroll" data-target="#navbar-main" class="background-image-auto bg_eco">
+	<body data-spy="scroll" data-target="#navbar-main" class="bg_eco">
 		<header> <?php include("navbar.html"); ?> </header>
 
 		<section id="theinsperationalquote" class="quote-block" style="">
