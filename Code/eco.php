a71dba4f5e0dca7678b613043b83bfcc9de1e56c
Orimius <55831728+Orimius@users.noreply.github.com>
2019-12-04 15:10:32 +0100

    Update eco.php

--- Code/eco.php
+++ Code/eco.php
@@ -33,14 +33,13 @@
 		<header class="margin" id="pagestart">
 		</header>
 
-		<section id="games" class="">
+		<section id="taketwomoney" class="">
 			<div class="container2 material-dark">
 				<div class="row">
 					<div class="col col-text">	
 						<h2>L'entreprise</h2>
-						<p class="lead">
+						<p class="lead" style="font-size: 1.25em; text-align: justify;">
 							L’entreprise Take-Two Interactive est cotée à la bourse de New-York, c’est une Limited Liability Company(LLC) ce qui est l'équivalent d’une Société À Responsabilité Limitée(SARL) en France. Ce type d’entreprise bénéficie de quelques avantages comme : 
-						
 							<ul>
 								<li>le choix de son éligibilité à l’imposition,</li>
 								<li> la responsabilité limitée protège, en fonction des lois sur la protection de l'État, les propriétaires, aussi appelé “membres” de l’entreprise, des actes et/ou dettes de la LLC,</li>
@@ -61,13 +60,13 @@
 			</div>
 		</section>
 
-		<section id="creators" class="">
+		<section id="concurrence" class="">
 			<div class="container2 material-dark">
 				<div class="row">
 					<div class="col col-img">
-						<img src="assets/img/test.png"/>
+						<img src="assets/img/bg_eco.jpg"/>
 					</div>
-					<div class="col col-text">	
+					<div class="col col-text" style="font-size: 1.25em; text-align: justify;">	
 						<h2>Quelques chiffres :</h2>
 						<p>
 							Take-Two Interactive c’est : 
@@ -111,12 +110,12 @@
 				</div>
 			</div>
 		</section>
-		<section id="thune" class="">
+		<section id="evolution" class="">
 			<div class="container2 material-dark">
 				<div class="row">
 					<div class="col col-text">	
 						<h2>L'évolution</h2>
-						<p class="lead">Take-Two Interactive est un des leaders du monde de la création de jeu vidéo, devançant comme vue ci-dessus Ubisoft et CD Projekt en termes de chiffre d’affaires. Ce géant doit son succès à des jeux phénomène tel que GTA V classé comme le 3ème jeu le plus vendu au monde avec plus de 110 millions de ventes depuis 2015, et bien d’autre licences que vous pouvez découvrir dans la partie “notre vitrine”.
+						<p class="lead" style="font-size: 1.23em; text-align: justify;">Take-Two Interactive est un des leaders du monde de la création de jeu vidéo, devançant comme vue ci-dessus Ubisoft et CD Projekt en termes de chiffre d’affaires. Ce géant doit son succès à des jeux phénomène tel que GTA V classé comme le 3ème jeu le plus vendu au monde avec plus de 110 millions de ventes depuis 2015, et bien d’autre licences que vous pouvez découvrir dans la partie “notre vitrine”.
 						l’entreprisereprise a connue une grande croissance au niveau du chiffre d’affaires et a battue des records de vente, l’avenir de Take-Two Interactive semble être dans la même direction que son évolution jusqu'à aujourd'hui, de plus une rumeur s'était répandue sur un potentiel achat de Take-Two Interactive par le géant Sony en mars 2019.
 
 						</p>
